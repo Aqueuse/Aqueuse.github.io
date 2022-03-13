9688f7692ebf6f03529fa76e8453389f0da75aee
Laura Bécognée <jaimaille@gmail.com>
2022-03-13 10:56:16 +0100

    finish a basic burger nav for the phone version :)

--- static/script.js
+++ static/script.js
@@ -1,3 +1,26 @@
 function showHideNavBar() {
-    document.getElementById("navbar").
+    if (document.getElementById("navbar").style.width === "80vw") {
+        document.getElementById("navbar").style.width = "0vw";
+
+        document.getElementById("navbar").childNodes.forEach(
+            e => {
+                if (e.nodeName === "SPAN") {
+                    e.style.display = "none";
+                }
+            }
+        )
+        return;
+    }
+
+    if (document.getElementById("navbar").style.width === "0vw") {
+        document.getElementById("navbar").style.width = "80vw";
+
+        document.getElementById("navbar").childNodes.forEach(
+            e => {
+                if (e.nodeName === "SPAN") {
+                    e.style.display = "block";
+                }
+            }
+        )
+    }
 }
