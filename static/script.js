cab15bcf1605010ceaa182078399ef8d7b9822a2
Laura Bécognée <jaimaille@gmail.com>
2022-03-18 11:50:30 +0100

    add a better top button (js) + maakyno CSS burger menu 🍔 ❤

--- static/script.js
+++ static/script.js
@@ -1,18 +1,22 @@
-// let isPhoneDisplay = window.matchMedia('(max-width: 767px)');
-//
-// if (isPhoneDisplay) {
-//     document.getElementById("navbar").style.zIndex = "-10";
-//     document.getElementById("navbar").style.width = "80vw";
-// }
-//
-// function showHideNavBar() {
-//     if (document.getElementById("navbar").style.zIndex === "-10") {
-//         document.getElementById("navbar").style.zIndex = "10";
-//
-//         return;
-//     }
-//
-//     if (document.getElementById("navbar").style.zIndex === "10") {
-//         document.getElementById("navbar").style.zIndex = "-10";
-//     }
-// }
+let burger = document.getElementById('burger');
+let header = document.querySelector('.navbar-off');
+let navElement = document.querySelector(".burger").nextElementSibling.querySelectorAll("span");
+
+let topButton = document.querySelector('.topButton');
+
+function toggleNav(){
+    header.classList.toggle('navbar');
+}
+
+burger.addEventListener('click', toggleNav);
+
+navElement.forEach(element => {
+    element.addEventListener('click', toggleNav);
+})
+
+// show/hide top button depending of the scroll Y position
+window.onscroll = function() {
+    let top = window.scrollY;
+    if (top >= 100) topButton.style.display="inline-block";
+    else topButton.style.display="none";
+}
